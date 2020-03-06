ca024f878f25e6c1dabd1bad5173ee65568d9351
Andrew Frazier <acorbettfrazier@gmail.com>
2020-03-06 15:52:13 -0500

    set new style to map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, Component } from "react";
-import Mapbox from "mapbox-gl";
 import "./App.css";
 import mapboxgl from "mapbox-gl";
+import axios from "axios";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiYW5kcmV3LWZyYXppZXI2IiwiYSI6ImNrN2djajVocTAwN3Ezb3RkY2xldGllM2gifQ.zd_rl93xQrHDhFddfdpuLQ";
@@ -11,18 +11,23 @@ export default class App extends Component {
     super(props);
     this.state = {
       lng: -75.409,
-      lat: 42.7986,
-      zoom: 5.7
+      lat: 43.2986,
+      zoom: 6.5
     };
   }
   componentDidMount() {
     const map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: "mapbox://styles/mapbox/streets-v11",
+      style: "mapbox://styles/andrew-frazier6/ck7gnb8tg19gf1ip4wcgigwbg",
       center: [this.state.lng, this.state.lat],
       zoom: this.state.zoom
     });
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
+      const fishingSpots = res.data;
+      this.setState({ persons });
+    });
   }
+
   render() {
     return (
       <div>
