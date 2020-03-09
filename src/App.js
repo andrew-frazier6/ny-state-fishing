602f2a26a359bcfee3e372ce8d7f2c429e41fdae
Andrew Frazier <acorbettfrazier@gmail.com>
2020-03-09 00:11:05 -0400

    connected to api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, Component } from "react";
 import "./App.css";
 import mapboxgl, { Marker } from "mapbox-gl";
-// import axios from "axios";
+import axios from "axios";
 import data from "./data.json";
 
 mapboxgl.accessToken =
@@ -18,41 +18,34 @@ export default class App extends Component {
     };
   }
   componentDidMount() {
+    this.getData();
+  }
+
+  placeMarkers = () => {
     const map = new mapboxgl.Map({
       container: this.mapContainer,
       style: "mapbox://styles/andrew-frazier6/ck7gnb8tg19gf1ip4wcgigwbg",
       center: [this.state.lng, this.state.lat],
       zoom: this.state.zoom
     });
-    var Markers = new mapboxgl.Marker()
-      .setLngLat([-75.946453998, 43.735181544])
-      .addTo(map);
-
-    var Marker = data.map((item, i) => {
+    var Marker = this.state.markers.map((item, i) => {
       new mapboxgl.Marker()
         .setLngLat([item.location.longitude, item.location.latitude])
         .addTo(map);
     });
-  }
-  // }
-  // axios.get("http://localhost:8000/").then(res => {
-  //   console.log(this.state.markers);
-  //   this.setState({ markers: res.data });
-  //   // console.log(markers);
-  // });
-  // }
+  };
+
+  getData = () => {
+    axios.get("https://newyork-fish-api6.herokuapp.com/").then(res => {
+      this.setState({ markers: res.data }, () => this.placeMarkers());
+    });
+  };
 
   render() {
+    console.log(this.state.marker);
     return (
       <div>
         <div ref={el => (this.mapContainer = el)} className="mapContainer" />
-        {/* <Marker>
-          <div>
-            key={data.PerformanceMark._id}
-            latitude={data.location.latitude}
-            longitude={data.location.longitude}
-          </div>
-        </Marker> */}
       </div>
     );
   }
