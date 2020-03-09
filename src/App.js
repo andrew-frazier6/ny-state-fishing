25f7899265ff72f056ff6e13c81e3aa65116901c
Andrew Frazier <acorbettfrazier@gmail.com>
2020-03-08 23:31:44 -0400

    implementation of markers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, Component } from "react";
 import "./App.css";
-import mapboxgl from "mapbox-gl";
-import axios from "axios";
+import mapboxgl, { Marker } from "mapbox-gl";
+// import axios from "axios";
+import data from "./data.json";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiYW5kcmV3LWZyYXppZXI2IiwiYSI6ImNrN2djajVocTAwN3Ezb3RkY2xldGllM2gifQ.zd_rl93xQrHDhFddfdpuLQ";
@@ -10,6 +11,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      markers: [],
       lng: -75.409,
       lat: 43.2986,
       zoom: 6.5
@@ -22,16 +24,35 @@ export default class App extends Component {
       center: [this.state.lng, this.state.lat],
       zoom: this.state.zoom
     });
-    axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
-      const fishingSpots = res.data;
-      this.setState({ persons });
+    var Markers = new mapboxgl.Marker()
+      .setLngLat([-75.946453998, 43.735181544])
+      .addTo(map);
+
+    var Marker = data.map((item, i) => {
+      new mapboxgl.Marker()
+        .setLngLat([item.location.longitude, item.location.latitude])
+        .addTo(map);
     });
   }
+  // }
+  // axios.get("http://localhost:8000/").then(res => {
+  //   console.log(this.state.markers);
+  //   this.setState({ markers: res.data });
+  //   // console.log(markers);
+  // });
+  // }
 
   render() {
     return (
       <div>
         <div ref={el => (this.mapContainer = el)} className="mapContainer" />
+        {/* <Marker>
+          <div>
+            key={data.PerformanceMark._id}
+            latitude={data.location.latitude}
+            longitude={data.location.longitude}
+          </div>
+        </Marker> */}
       </div>
     );
   }
---
+++ src/marker/marker.js
@@ -0,0 +1,18 @@
+// import React from "react";
+
+// function marker() {
+//   return <div></div>;
+// }
+
+// export default marker;
+
+// var marker = new mapboxgl.Marker()
+//   .setLngLat([])
+//   .addTo(map);
+
+// import axios from "axios";
+
+// axios.get(`localhost:8000/`).then(res => {
+//   const fishingSpots = res.data;
+//   this.setState.markers({ fishingSpots.map() });
+// });
